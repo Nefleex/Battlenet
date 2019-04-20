8f5428e1b5c4deec64ba936fad9f2677b862a5d0
Mika Knuuttila <mika.knuuttila@hotmail.com>
2019-04-20 15:40:14 +0300

    Routes now allowed for delete and update methods

--- auctions/middleware/headers.js
+++ auctions/middleware/headers.js
@@ -1,5 +1,6 @@
 const headers = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token,Authorization"
